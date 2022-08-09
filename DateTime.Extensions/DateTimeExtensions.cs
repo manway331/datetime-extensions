7f12677da84993f175c7832015b1f1b32c85b77a
Marco Man <manway331@gmail.com>
2022-08-09 09:22:37 +0800

    first commit

---
+++ DateTime.Extensions/DateTimeExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace DateTimeExtensions
+{
+    public static class DateTimeExtensions
+    {
+
+        public static bool IsEqualDay(this DateTime dateTime1, DateTime dateTime2)
+        {
+            return (
+                dateTime1.Year == dateTime2.Year &&
+                dateTime1.Month == dateTime2.Month &&
+                dateTime1.Day == dateTime2.Day
+              );
+        }
+    }
+}
+
---
+++ DateTime.Extensions/DayOfWeekExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace DateTimeExtensions
+{
+    public class DayOfWeekExtensions
+    {
+        public DayOfWeekExtensions()
+        {
+        }
+    }
+}
+
